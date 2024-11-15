98ae9aaa1ea780551d181cc17cdbc161c84c9cd6
Atago <wwwshehuigan@sina.com>
2024-11-15 14:35:54 +0800

    registerForActivityResult不能在oncreate之后的状态下创建

--- app/src/main/java/com/example/xianyuplayer/LocalFileFragment.kt
+++ app/src/main/java/com/example/xianyuplayer/LocalFileFragment.kt
@@ -7,8 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
@@ -19,6 +18,23 @@ class LocalFileFragment : Fragment() {
     private val TAG = "LocalFileFragment"
 
     private lateinit var binding: FragmentLocalFileBinding
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val treeUri = result.data!!.data!!
+                requireContext().contentResolver.takePersistableUriPermission(treeUri, Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                val documentFile = DocumentFile.fromTreeUri(requireContext(), treeUri)
+                val listFiles = documentFile!!.listFiles()
+                for (listFile in listFiles) {
+                    Log.i(TAG, "onCreate: --> file uri ${listFile.uri}")
+                }
+            }
+        }
+
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -30,18 +46,11 @@ class LocalFileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.btnScanFile.setOnClickListener {
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-            intent.setDataAndType(null, "*/*")
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback<ActivityResult> { result ->
 
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val treeUri = result.data!!.data!!
-                    requireContext().contentResolver.takePersistableUriPermission(treeUri, Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                    val documentFile = DocumentFile.fromTreeUri(requireContext(), treeUri)
-                    val listFiles = documentFile!!.listFiles()
-                    Log.i(TAG, "onViewCreated: -->${listFiles.toString()}")
-                }
-            })
+            if (::launcher.isInitialized) {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+                launcher.launch(intent)
+            }
         }
     }
 }
